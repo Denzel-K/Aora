a94ce8eb3d2df93cda9ed289eead329763754338
Denzel <denzelk741@gmail.com>
2024-08-05 12:11:29 +0300

    Initial commit_Folder structure setup

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const AuthLayout = () => {
+  return (
+    <View>
+      <Text>_layout</Text>
+    </View>
+  )
+}
+
+export default AuthLayout;
---
+++ app/(auth)/sign_in.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const SignIn = () => {
+  return (
+    <View>
+      <Text>sign_in</Text>
+    </View>
+  )
+}
+
+export default SignIn;
